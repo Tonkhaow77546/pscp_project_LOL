913902ca6beee19497964ca17eae3ee0b14df317
pongpisut <145266199+pongpisuttuwpong@users.noreply.github.com>
2023-11-12 15:33:22 +0700

    Update final UI .py
    improve code

--- final game/final UI (maybe).py
+++ final game/final UI (maybe).py
@@ -28,14 +28,6 @@ BG = pygame.transform.scale(BG, (1920, 1060))
 botton_play = pygame.image.load("UI/Start_2.png")
 botton_play = pygame.transform.scale(botton_play, (300,80))
 
-## bottonmode
-botton_mode = pygame.image.load("UI/Mode.png") 
-botton_mode = pygame.transform.scale(botton_mode, (300,500))
-
-## bottonsetting
-botton_setting = pygame.image.load("UI/Setting.png")
-botton_setting = pygame.transform.scale(botton_setting, (80,100))
-
 ## bottoncredit
 botton_credit = pygame.image.load("UI/Credits_2.png")
 botton_credit = pygame.transform.scale(botton_credit, (180,60))
@@ -43,58 +35,6 @@ botton_credit = pygame.transform.scale(botton_credit, (180,60))
 ## EjudgeAvatar
 ejudge_avatar = pygame.image.load("UI/LOL Logo.png")
 ejudge_avatar = pygame.transform.scale(ejudge_avatar, (800,350))
-
-def playgame():
-    """playgame"""
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("Gray")
-
-        OPTIONS_TEXT = get_font(45).render("This is game", True, "Aquamarine")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        OPTIONS_BACK = Button(image=pygame.image.load("UI/Quit Rect.png"), pos=(640, 460), 
-                            text_input="Back", font=get_font(75), base_color="Blue", hovering_color="Green")
-
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
-    
-def setting():
-    """settingmenu"""
-    while True:
-        OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
-
-        SCREEN.fill("Gray")
-
-        OPTIONS_TEXT = get_font(45).render("setting menu", True, "Aquamarine")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
-        SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
-
-        OPTIONS_BACK = Button(image=pygame.image.load("UI/Quit Rect.png"), pos=(640, 460), 
-                            text_input="Back", font=get_font(75), base_color="Blue", hovering_color="Green")
-
-        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
-        OPTIONS_BACK.update(SCREEN)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
-                    main_menu()
-        pygame.display.update()
 
 def credit():
     """credit"""
@@ -136,13 +76,9 @@ def main_menu():
         MENU_TEXT = get_font(50).render("", True, "White")
         MENU_RECT = MENU_TEXT.get_rect(center=(960, 500))
         
-        PLAY_BUTTON = Button(botton_play , pos=(960, 700),  ##ปุ่ม start size < 400 x 200 , > 250 x 70
+        PLAY_BUTTON = Button(botton_play , pos=(960, 700),  ##ปุ่ม start
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="Black")
-        MODE_BUTTON = Button(botton_mode, pos=(640, 620), #ปุ่ม option size < ที่ใส่ไปแล้วไม่น่าเกลียด
-                            text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="Black")
-        SETTING_BUTTON = Button(botton_setting, pos=(1200, 60), #ปุ่ม option size < ที่ใส่ไปแล้วไม่น่าเกลียด
-                            text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="Black")
-        CREDIT_BUTTON = Button(botton_credit, pos=(125, 60), #ปุ่ม option size < ที่ใส่ไปแล้วไม่น่าเกลียด
+        CREDIT_BUTTON = Button(botton_credit, pos=(125, 60), #ปุ่ม credit
                             text_input="", font=get_font(75), base_color="#d7fcd4", hovering_color="Black")
         
         SCREEN.blit(MENU_TEXT, MENU_RECT)
